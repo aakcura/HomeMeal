c0ff279ff92adf95466a19f8698771ef835c44a7
Arin Akcura <aakcura2001@gmail.com>
2019-08-16 02:18:50 +0300

    Customer -> MealListTableViewCell created

---
+++ HomeMeal/Customer/MealList/MealListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealListTableViewCell.swift
+//  HomeMeal
+//
+//  Created by Batuhan Abay on 16.08.2019.
+//  Copyright © 2019 Arin Akcura. All rights reserved.
+//
+
+import UIKit
+
+class MealListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
